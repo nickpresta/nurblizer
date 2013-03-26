dcf55e6c27ae11abcda06f1c00d19f9f08571e79
Nick Presta <nick@nickpresta.ca>
2013-03-25 23:38:30 -0400

    Initial commit for Nurblizer.

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
+var templates = template.Must(template.ParseFiles("index.html", "nurble.html"))
+
+func mainRequestHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", "")
+}
+
+func nurbleRequestHandler(w http.ResponseWriter, r *http.Request) {
+	text := r.FormValue("text")
+	text = nurble(text)
+	safe := template.HTMLEscapeString(text)
+	safe = strings.Replace(safe, "\n", "<br>", -1)
+	templates.ExecuteTemplate(w, "nurble.html", template.HTML(safe))
+}
+
+func nurble(text string) string {
+	upper := strings.ToUpper(text)
+	words := wordRegexp.ReplaceAllString(upper, "")
+	splitWords := strings.Fields(words)
+
+	for _, word := range splitWords {
+		if NOUNS[word] == false {
+			pattern := regexp.MustCompile(word)
+			upper = pattern.ReplaceAllString(upper, "nurble")
+		}
+	}
+
+	return upper
+}
+
+func readNouns() {
+	file, err := ioutil.ReadFile("./nouns.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parts := bytes.Split(file, []byte{'\n'})
+	NOUNS = make(map[string]bool, len(parts))
+	for _, line := range parts {
+		NOUNS[strings.ToUpper(string(line))] = true
+	}
+}
+
+var (
+	NOUNS      map[string]bool
+	wordRegexp = regexp.MustCompile("[^A-Z ]")
+)
+
+func main() {
+	numCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPU)
+
+	readNouns()
+
+	http.HandleFunc("/", mainRequestHandler)
+	http.HandleFunc("/nurble", nurbleRequestHandler)
+	err := http.ListenAndServe(":8080", http.DefaultServeMux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
